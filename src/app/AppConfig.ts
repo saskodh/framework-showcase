b4daff92619c191ef043f52f75e97264f601dbf8
Damjan Angelovski <damjangelovski@gmail.com>
2016-07-25 17:26:43 +0200

    [no-issue] Updated @PropertySource() to lazy load the properties - Feature provided within this issue https://github.com/saskodh/framework/issues/27 [reviewed by @saskodh and @draganAndonovski]

--- src/app/AppConfig.ts
+++ src/app/AppConfig.ts
@@ -1,10 +1,9 @@
-import {Configuration, PropertySource, Import} from "@sklechko/framework";
+import { Configuration, PropertySource, Import } from "@sklechko/framework";
 import {ControllersConfig} from "./controllers/ControllersConfig";
 import {RepositoriesConfig} from "./repositories/RepositoriesConfig";
 import {ServicesConfig} from "./services/ServicesConfig";
-var properties = require('../resources/app.properties.json');
 
 @Import(ControllersConfig, RepositoriesConfig, ServicesConfig)
-@PropertySource(properties)
+@PropertySource(__dirname+'/../resources/app.properties.json')
 @Configuration()
 export class AppConfig {}
