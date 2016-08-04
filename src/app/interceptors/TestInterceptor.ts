e03a883b7b18fb6c0a8ac6db61ea749aa63405bc
Damjan Angelovski <damjangelovski@gmail.com>
2016-08-04 15:29:49 +0200

    [no-issue] changed the example to reflect the framework commit - added afterCompletion method on TestInterceptor

--- src/app/interceptors/TestInterceptor.ts
+++ src/app/interceptors/TestInterceptor.ts
@@ -9,11 +9,20 @@ export class TestInterceptor implements Interceptor {
         if(request.originalUrl === '/hi'){
             response.preHandleProperty = 'interceptor preHandling works!';
         }
+        //response.json({'someObjectKey': 'someVal'});
+        //return false;
     }
 
     postHandle(request, response) {
         if(request.originalUrl === '/hi'){
-            response.result.postHandleProperty = 'interceptor postHandling works!';
+            //response.result.postHandleProperty = 'interceptor postHandling works!';
+            response.$$frameworkData.model.postHandleProperty = 'interceptor postHandling works!';
+        }
+    }
+
+    afterCompletion(request, response) {
+        if(request.originalUrl === '/hi'){
+            console.log('interceptor afterCompletion works!');
         }
     }
 
